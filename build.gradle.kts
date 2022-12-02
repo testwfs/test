00fa21d302631409b96368a8eb7ff24f1d92b83f
Rocio Sanchez Fernandez <rocio.sanchezfernandez@webfleet.com>
2022-12-02 14:42:36 +0100

    First test

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.4"
+	id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+java.targetCompatibility = JavaVersion.VERSION_11
+
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
