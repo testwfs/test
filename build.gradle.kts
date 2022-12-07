972de7fe69c9983d7e75ba0d794465a40fdbecbe
Sven Apel <sven.apel@webfleet.com>
2022-12-07 12:00:08 +0100

    TEst push

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.example"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 java.targetCompatibility = JavaVersion.VERSION_11
 
